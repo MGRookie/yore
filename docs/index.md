d894641002862712a91b711bf408f45dbf697866
gaoluo <574169815@qq.com>
2023-11-30 23:35:12 +0800

    init

---
+++ docs/index.md
@@ -0,0 +1,12 @@
+---
+pageType: home
+
+hero:
+  name: Yore
+  text: 言念君子，温其如玉
+  tagline: ''
+  actions:
+    - theme: brand
+      text: 开始
+      link: '/rust/'
+---
---
+++ rspress.config.ts
@@ -0,0 +1,18 @@
+import * as path from 'path';
+import { defineConfig } from 'rspress/config';
+
+export default defineConfig({
+  root: path.join(__dirname, 'docs'),
+  title: 'In Days of Yore',
+  description: '言念君子，温其如玉',
+  icon: '/rspress-icon.png',
+  logo: {
+    light: '/rspress-light-logo.png',
+    dark: '/rspress-dark-logo.png',
+  },
+  themeConfig: {
+    socialLinks: [
+      { icon: 'github', mode: 'link', content: 'https://github.com/' },
+    ],
+  },
+});
